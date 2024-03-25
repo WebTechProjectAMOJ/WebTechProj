be396213d93142b1bcba6a4742b25e64f77c4386
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-03-25 17:13:12 +0100

    Add Model.Restaurant

---
+++ src/main/java/model/Restaurant.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.io.Serializable;
+
+public class Restaurant implements Serializable {
+    public String name;
+    public String address;
+
+    public Restaurant() {
+    }
+}
