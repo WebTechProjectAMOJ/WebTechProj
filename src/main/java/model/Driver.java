5d1cfeeebb2ce4001bc8f58fc0a121681ac7b5f0
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-03-25 17:12:08 +0100

    Add Model.Driver

---
+++ src/main/java/model/Driver.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.io.Serializable;
+
+public class Driver implements Serializable {
+    public String name;
+    public Double rating;
+
+    public Driver() {
+    }
+}
