92874277bccded7b61a791e890b4de249ca61874
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-03-25 17:13:49 +0100

    Add Model.Tag

---
+++ src/main/java/model/Tag.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.io.Serializable;
+
+public class Tag implements Serializable {
+
+    public String name;
+    public Boolean preference;
+
+    public Tag() {
+    }
+}
