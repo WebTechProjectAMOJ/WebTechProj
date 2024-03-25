e74f73af2af09487d3d31b9724e027a0610571cf
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-03-25 17:12:29 +0100

    Add Model.Member

---
+++ src/main/java/model/Member.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+
+    public String name;
+    public Integer telephone;
+    public String email;
+    public String[] addresses;
+
+    public Member() {
+    }
+
+}
