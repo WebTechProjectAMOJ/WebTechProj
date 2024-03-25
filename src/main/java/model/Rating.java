fa65ea5c7f4b458f0c95ea5f57523de8565fbb99
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-03-25 17:13:02 +0100

    Add Model.Rating

---
+++ src/main/java/model/Rating.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.io.Serializable;
+
+public class Rating implements Serializable {
+    public String feedback;
+    public double rating;
+
+    public Rating() {
+    }
+}
