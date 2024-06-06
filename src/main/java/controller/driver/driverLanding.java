064adeb12d8285f4b5c51c7c1a31fdaaad1bf1bc
orionvi <15966899+OrionVi1998@users.noreply.github.com>
2024-06-06 22:06:46 +0200

    add driver-pending

--- src/main/java/controller/driver/driverLanding.java
+++ src/main/java/controller/driver/driverLanding.java
@@ -18,21 +18,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-@WebServlet(name = "Driver Landing", value = "/driver-landing")
+@WebServlet(name = "Driver Landing", urlPatterns = {"/driver-landing", "/driver-pending"})
 public class driverLanding extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         HttpSession session = req.getSession(false);
-        if (session == null ||  session.getAttribute("accountType") != "driver") {
+        if (session == null || session.getAttribute("accountType") != "driver") {
             resp.sendRedirect(req.getContextPath() + "/");
             return;
         }
+
+        String url = req.getRequestURI().split("/")[req.getRequestURI().split("/").length - 1];
+
         Driver driver = (Driver) session.getAttribute("user");
 
         // Makes a hashmap with list of all status and a list of all orders
-        HashMap<String, ArrayList<ItemBoxUi>> order_hash = driver.getUIHashOrderStatus();
+        HashMap<String, ArrayList<ItemBoxUi>> order_hash = null;
 
-        System.out.println(order_hash);
+        if (url.equals("driver-landing")) {
+            order_hash = driver.getUIHashOrderStatus();
+        }
+
+        if (url.equals("driver-pending")) {
+            order_hash = driver.get_not_complete_orders();
+        }
 
         // Sets attributes for the view
         req.setAttribute("user", driver);
