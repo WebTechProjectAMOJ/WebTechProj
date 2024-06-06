c2e599e8f2cce55f97d21e679b51b3a8f660047d
orionvi <15966899+OrionVi1998@users.noreply.github.com>
2024-06-06 16:37:34 +0200

    Extract repeating code into Consumer and User

--- src/main/java/controller/consumer/consumerLanding.java
+++ src/main/java/controller/consumer/consumerLanding.java
@@ -1,6 +1,5 @@
 package controller.consumer;
 
-import dbconnection.DbConnection;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,19 +7,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import models.order.Order;
-import models.tags.Tag;
 import models.ui_util.ItemBoxUi;
 import models.user.Consumer;
-import models.user.Restaurant;
-import org.bson.Document;
-import org.bson.types.ObjectId;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Objects;
 
 
 @WebServlet(name = "Consumer Landing", value = "/customer-landing")
@@ -34,74 +26,10 @@ public class consumerLanding extends HttpServlet {
             return;
         }
 
-        // Create the tags to display
+        Consumer user = (Consumer) session.getAttribute("user");
 
-        Document to_find = new Document("type", "preference");
-        ArrayList<Document> found = DbConnection.find(
-                "tags",
-                to_find
-        );
-
-        HashMap<String, ArrayList<ItemBoxUi>> resto_hash = new HashMap<String, ArrayList<ItemBoxUi>>();
-        for (Document tag_doc : found) {
-            Tag tag = new Tag(tag_doc);
-
-            Document resto_find = new Document("tags", tag.getId());
-            ArrayList<Document> found_resto_docs = DbConnection.find(
-                    "restaurants",
-                    resto_find
-            );
-
-            ArrayList<ItemBoxUi> restos_filtered = new ArrayList<ItemBoxUi>();
-            for (Document rest_doc : found_resto_docs) {
-                Restaurant resto = new Restaurant(rest_doc);
-                restos_filtered.add(resto.getUiItemBox());
-            }
-
-            if (resto_hash.get(tag.getName()) != null) {
-                resto_hash.get(tag.getName()).addAll(restos_filtered);
-            } else {
-                ArrayList<ItemBoxUi> new_cat = new ArrayList<ItemBoxUi>(restos_filtered);
-                resto_hash.put(tag.getName(), new_cat);
-            }
-        }
-
-        ArrayList<String> empty_tags = new ArrayList<>();
-
-        for (String tag_name : resto_hash.keySet()) {
-            if (resto_hash.get(tag_name).isEmpty()) {
-                empty_tags.add(tag_name);
-            }
-        }
-
-        for (String tag_name : empty_tags) {
-            resto_hash.remove(tag_name);
-        }
-
-        // Create the recent Orders collection
-
-        to_find = new Document("status", "complete");
-        found = DbConnection.find(
-                "orders",
-                to_find
-        );
-
-        HashMap<String, ArrayList<ItemBoxUi>> orders_scroll = new HashMap<String, ArrayList<ItemBoxUi>>();
-
-        for (Document order_doc : found) {
-            Order order = new Order(order_doc);
-
-            if (Objects.equals(order.getStatus(), "complete")) {
-                if (orders_scroll.get("Recent Orders") != null) {
-                    orders_scroll.get("Recent Orders").add(order.getUiItemBox());
-                    Collections.reverse(orders_scroll.get("Recent Orders"));
-                } else {
-                    ArrayList<ItemBoxUi> new_cat = new ArrayList<ItemBoxUi>();
-                    new_cat.add(order.getUiItemBox());
-                    orders_scroll.put("Recent Orders", new_cat);
-                }
-            }
-        }
+        HashMap<String, ArrayList<ItemBoxUi>> resto_hash = user.get_restaurant_options();
+        HashMap<String, ArrayList<ItemBoxUi>> orders_scroll = user.get_recent_orders();
 
 
         // Sets attributes for the view
--- src/main/java/controller/consumer/consumerOngoing.java
+++ src/main/java/controller/consumer/consumerOngoing.java
@@ -8,19 +8,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import models.order.Order;
-import models.tags.Tag;
 import models.ui_util.ItemBoxUi;
 import models.user.Consumer;
-import models.user.Restaurant;
-import org.bson.Document;
-import org.bson.types.ObjectId;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Objects;
 
 
 @WebServlet(name = "Consumer Ongoing", value = "/customer-orders-ongoing")
@@ -34,29 +27,9 @@ public class consumerOngoing extends HttpServlet {
             return;
         }
 
-        // Create the tags to display
+        Consumer user = (Consumer) session.getAttribute("user");
+        HashMap<String, ArrayList<ItemBoxUi>> orders_scroll = user.get_not_complete_orders();
 
-        Document to_find = new Document("status", "complete");
-        ArrayList<Document> found = DbConnection.find_with_not_filter(
-                "orders",
-                to_find
-        );
-
-        HashMap<String, ArrayList<ItemBoxUi>> orders_scroll = new HashMap<String, ArrayList<ItemBoxUi>>();
-
-        // Create the Orders collection
-
-        for (Document order_doc : found) {
-            Order order = new Order(order_doc);
-
-            if (orders_scroll.get(order.getStatus()) != null) {
-                orders_scroll.get(order.getStatus()).add(order.getUiItemBox());
-            } else {
-                ArrayList<ItemBoxUi> new_cat = new ArrayList<ItemBoxUi>();
-                new_cat.add(order.getUiItemBox());
-                orders_scroll.put(order.getStatus(), new_cat);
-            }
-        }
 
         // Sets attributes for the view
         req.setAttribute("orders_to_scroll", orders_scroll);
