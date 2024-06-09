b262cf83bc32849e59012035360931d197d5c77d
Aaron M <aaron.g.michael@gmail.com>
2024-06-09 11:21:50 +0200

    Fixing js for login page

--- src/main/webapp/js/design/toggleActiveLoginTab.js
+++ src/main/webapp/js/design/toggleActiveLoginTab.js
@@ -1,25 +1,22 @@
-// $('.login-tabs > div').on('click', function () {
-//     $(this).children('input')[0].checked = true
-//     $('input[name = "account-type"]').each(function () {
-//         $(this).parent('div').eq(0).removeClass('tab-active')
-//     })
-//     $(this).toggleClass('tab-active')
-// })
-document.querySelectorAll('.login-tabs > div').forEach(x => {
-    if(!x.classList.contains('tab-active')){
-        x.onclick = function (){
-            let text = x.innerText.toLowerCase();
-            document.getElementById('login-cloud').classList.remove('consumer-box', 'restaurants-box', 'driver-box');
-            if (text === 'restaurants') {
-                document.getElementById('login-cloud').classList.add('restaurants-box');
-            } else if (text === 'drivers') {
-                document.getElementById('login-cloud').classList.add('driver-box');
-            } else {
-                document.getElementById('login-cloud').classList.add('consumer-box');
-            }
-            document.querySelector('.tab-active').classList.remove('tab-active');
-            x.classList.add('tab-active');
+$('.login-tabs > div').on('click', function () {
+    $(this).children('input')[0].checked = true
+    $('input[name = "account-type"]').each(function () {
+        $(this).parent('div').eq(0).removeClass('tab-active')
+    })
+    $(this).toggleClass('tab-active', true)
+    let text = $(this).text().trim();
+    document.querySelector('div.container').classList.remove('consumer-box', 'restaurants-box', 'driver-box');
+    switch(text.toLowerCase()) {
+        case "restaurants": {
+            $("div.container").toggleClass("restaurants-box", true);
+            break;
+        }
+        case "drivers": {
+            $("div.container").toggleClass("driver-box", true);
+            break;
+        }
+        default:{
+            $("div.container").toggleClass("consumer-box", true);
         }
     }
-});
-
+})
